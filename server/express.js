f294c24b8fc31c01d0f2bafd922d9ad997571b7d
Carlos Malissia <116571296+geniototal@users.noreply.github.com>
2024-07-06 14:17:20 -0300

    Update express.js

--- server/express.js
+++ server/express.js
@@ -50,8 +50,8 @@ app.options("*", cors(corsOptions));
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    ,"http://localhost:3000",
-    "https://pf-henry-15a-ecommerce-frontend.vercel.app/"
+    "http://localhost:3000",
+    "https://pf-henry-15a-ecommerce-frontend.vercel.app"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
